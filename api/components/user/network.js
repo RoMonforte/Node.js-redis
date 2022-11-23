47a0ebe771abf353114fffeda28e6a7c826439be
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-11-23 03:07:31 -0600

    dummy db

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const response = require('../../../network/response');
+const Controller = require('./controller');
 
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    response.sucess(req, res, 'Todo correcto', 200);
+    const list = Controller.list();
+    response.sucess(req, res, list, 200);
 });
 
 module.exports = router;
