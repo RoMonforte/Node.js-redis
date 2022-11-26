93978bdc6470259ba7a4d702a2527df114e0f00d
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-11-26 06:12:04 -0600

    auth with jwt

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,13 +1,48 @@
 const express = require('express');
 
 const response = require('../../../network/response');
-const Controller = require('./controller');
+const Controller = require('./index');
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    const list = Controller.list();
-    response.sucess(req, res, list, 200);
-});
+// Routes
+router.get('/', list)
+router.get('/:id', get);
+router.post('/', upsert);
+router.put('/', upsert);
+
+// Internal functions
+function list(req, res) {
+    Controller.list()
+        .then((lista) => {
+            response.sucess(req, res, lista, 200);
+        })
+        .catch((err) => {
+            response.error(req, res, err.message, 500);
+        });
+    
+}
+
+function get(req, res) {
+    Controller.get(req.params.id)
+        .then((user) => {
+            response.sucess(req, res, user, 200);
+        })
+        .catch((err) => {
+            response.error(req, res, err.message, 500);
+        });
+    
+}
+
+function upsert(req, res) {
+    Controller.upsert(req.body)
+        .then((user) => {
+            response.sucess(req, res, user, 201);
+        })
+        .catch((err) => {
+            response.error(req, res, err.message, 500);
+        });
+    
+}
 
 module.exports = router;
--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const config = require('../config.js');
 const user = require('./components/user/network');
+const auth = require('./components/auth/network');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 //ROUTES
 app.use('/api/user', user);
+app.use('/api/auth', auth);
 
 app.listen(config.api.port, () => {
     console.log('Escuchando en el puerto ' + config.api.port);
