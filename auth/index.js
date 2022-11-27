50fd6b9bdd9912cc7208c0f37d839e5dc22e9d8f
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-11-27 05:54:19 -0600

    jwt to put action

---
+++ auth/index.js
@@ -0,0 +1,52 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const secret = config.jwt.secret;
+
+function sign(data) {
+    return jwt.sign(data, secret)
+}
+
+function verify(token) {
+    return jwt.verify(token, secret);
+}
+
+const check = {
+    own: function(req,owner) {
+        const decoded = decodeHeader(req);
+        console.log(decoded);
+
+        if(decoded.id !== owner) {
+            throw new Error('You cant do this!')
+        }
+    }
+}
+
+function getToken(auth) {
+    if(!auth) {
+        throw new Error('Token is not contained');
+    }
+
+    if (auth.indexOf('Bearer ') === -1) {
+        throw new Error('Invalid format');
+    }
+
+    let token = auth.replace('Bearer ', '');
+    
+    return token;
+}
+
+function decodeHeader(req) {
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decoded = verify(token);
+
+    req.user = decoded;
+
+    return decoded;
+}
+
+
+module.exports = {
+    sign,
+    check
+};
